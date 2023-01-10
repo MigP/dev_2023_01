481f132cb2858efa76fdb49d32ae734087314f79
Miguel Pinto <mig06@hotmail.co.uk>
2023-01-10 19:20:33 +0100

    Article details activity layout created

--- app/src/main/java/bf/be/android/newsapptest/view/ArticleActivity.kt
+++ app/src/main/java/bf/be/android/newsapptest/view/ArticleActivity.kt
@@ -3,7 +3,6 @@ package bf.be.android.newsapptest.view
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import bf.be.android.newsapptest.databinding.ActivityArticleBinding
-import bf.be.android.newsapptest.model.apis.Items
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class ArticleActivity : AppCompatActivity() {
@@ -15,31 +14,41 @@ class ArticleActivity : AppCompatActivity() {
         setContentView(binding.root)
         setTitle("My News")
 
+        populateList()
+
         val backButton: FloatingActionButton = binding.backFloatingActionButton
         backButton.setOnClickListener {
             finish()
         }
+    }
 
-        //TODO ----------------------------- Gets data from intent
-        val item_title = intent.getStringExtra("TITLE")
-        val item_date = intent.getStringExtra("DATE")
-        val item_page = intent.getStringExtra("PAGE")
-        val item_place_of_publication = intent.getStringExtra("PLACE_OF_PUBLICATION")
-        val item_language = intent.getStringArrayExtra("LANGUAGE")
-        val item_note = intent.getStringArrayExtra("NOTE")
-        val item_subject = intent.getStringArrayExtra("SUBJECT")
-        val item_frequency = intent.getStringExtra("FREQUENCY")
-        val item_type = intent.getStringExtra("TYPE")
-        val item_edition_label = intent.getStringExtra("EDITION_LABEL")
-        val item_publisher = intent.getStringExtra("PUBLISHER")
-        val item_url = intent.getStringExtra("URL")
-        val item_ocr_eng = intent.getStringExtra("OCR_ENG")
-        //TODO ------------------------------------------
+    private fun populateList () {
+        val item_title = binding.itemTitle
+        item_title.text = intent.getStringExtra("TITLE")
+        val item_date = binding.itemDate
+        item_date.text = intent.getStringExtra("DATE")
+        val item_place_of_publication = binding.itemPlace
+        item_place_of_publication.text = intent.getStringExtra("PLACE_OF_PUBLICATION")
+        val item_subject = binding.itemSubject
+        item_subject.text = intent.getSerializableExtra("SUBJECT").toString()
+        val item_language = binding.itemLanguage
+        item_language.text = intent.getSerializableExtra("LANGUAGE").toString()
+        val item_type = binding.itemType
+        item_type.text = intent.getStringExtra("TYPE")
+        val item_page = binding.itemPage
+        item_page.text = intent.getStringExtra("PAGE")
+        val item_frequency = binding.itemFrequency
+        item_frequency.text = intent.getStringExtra("FREQUENCY")
+        val item_edition_label = binding.itemLabel
+        item_edition_label .text = intent.getStringExtra("EDITION_LABEL")
+        val item_publisher = binding.itemPublisher
+        item_publisher.text = intent.getStringExtra("PUBLISHER")
+        val item_note = binding.itemNotes
+        item_note.text = intent.getSerializableExtra("NOTE").toString()
+        val item_ocr_eng = binding.itemOcr
+        item_ocr_eng.text = intent.getStringExtra("OCR_ENG")
+
         //TODO Also get pdf (original scan) from json response on the item_url request
-
-        println("-------------- title: " + item_title)
-        println("-------------- item_place_of_publication: " + item_place_of_publication)
-        println("-------------- item_frequency: " + item_frequency)
-        println("-------------- item_publisher: " + item_publisher)
+        //TODO Instead of displaying the Ocr add a link to it like on the pdf
     }
 }
